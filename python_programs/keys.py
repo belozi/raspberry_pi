022c60a30771c6e43259128c59deb38944633ba3
Robert Belozi Lee <belozi@localhost.localdomain>
2015-10-09 20:52:36 -0400

    adding .gitignore for keys to twitter

---
+++ python_programs/keys.py
@@ -0,0 +1,4 @@
+consumer_key = "195iO9vNPGcRH6m7vLZGDPPnU"
+consumer_secret = "5KcPqv5BITzIFjgxs5MgXj9KKcP8NkO3xWdyvzlvkv8hDNDqT3"
+access_token = "3047708060-LOo0zmKXOH9xhZKZQ7wzEmUQ3TY8iatmcr6YZcj"
+access_token_secret = "McAvNpDHw2eyqeojL8lJJ6XcBAqMKxc0OEEpCkj0dWar9"
--- python_programs/tweepy_tutorial.py
+++ python_programs/tweepy_tutorial.py
@@ -1,18 +1,15 @@
 import tweepy
+import keys
 
-consumer_key = 
-consumer_secret = 
-access_token = 
-access_token_secret = 
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret) 
+auth.set_access_token(keys.access_token, keys.access_token_secret)
 
 api = tweepy.API(auth)
     
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
-    print tweet.text
+    print tweet.text, "\n"
+    
      
 
 try:
